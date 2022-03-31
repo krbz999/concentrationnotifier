63b77ff3e1f9275c59c5f6fbff8d465b56898287
krbz999 <50169243+krbz999@users.noreply.github.com>
2022-03-31 21:02:36 +0200

    Update const.mjs

--- scripts/const.mjs
+++ scripts/const.mjs
@@ -1,3 +1,3 @@
-export const MODULE_NAME = "dicerecharge";
-export const MODULE_TITLE = "Zhell's Formulaic Recharging";
+export const MODULE_NAME = "concentrationnotifier";
+export const MODULE_TITLE = "Z's Concentration Notifier";
 export const MODULE_TITLE_SHORT = "ZHELL";
