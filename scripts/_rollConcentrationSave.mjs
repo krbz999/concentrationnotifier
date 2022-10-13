004e67699f12ed2fd5dfd8a9bf7e99ad50b46fbd
Kenneth B <kennethzhell@gmail.com>
2022-10-13 04:42:55 +0200

    Compatibility with babonus.

--- scripts/_rollConcentrationSave.mjs
+++ scripts/_rollConcentrationSave.mjs
@@ -4,10 +4,9 @@ export function setHooks_rollConcentrationSave() {
 
   /* If the saving throw is for concentration, add the bonus on top in the pre-hook. */
   Hooks.on("dnd5e.preRollAbilitySave", (actor, rollConfig, saveType) => {
-    if (foundry.utils.hasProperty(rollConfig, "isConcSave")) {
-      if (!!rollConfig.concentrationBonus) {
-        rollConfig.parts.push(rollConfig.concentrationBonus);
-      }
+    const { isConcSave, concentrationBonus } = rollConfig;
+    if (isConcSave && !!concentrationBonus) {
+      rollConfig.parts.push(...rollConfig.concentrationBonus);
     }
   });
 }
@@ -16,18 +15,24 @@ export function setHooks_rollConcentrationSave() {
 export const rollConcentrationSave = async function (ability, options = {}) {
   if (!this.isOwner) return;
 
-  let abl = ability ?? this.getFlag("dnd5e", "concentrationAbility");
+  const abl = ability ?? this.getFlag("dnd5e", "concentrationAbility");
 
-  let rollConfig = {
-    fumble: null,
-    critical: null,
-    event,
-    reliableTalent: this.getFlag("dnd5e", "concentrationReliable"),
-    advantage: this.getFlag("dnd5e", "concentrationAdvantage") && !event.ctrlKey,
-    isConcSave: true,
-    concentrationBonus: this.getFlag("dnd5e", "concentrationBonus")
-  }
-  rollConfig = foundry.utils.mergeObject(rollConfig, options);
+  const rollConfig = { fumble: null, critical: null, event, isConcSave: true };
+  const reliableTalent = this.getFlag("dnd5e", "concentrationReliable");
+  if (reliableTalent) rollConfig.reliableTalent = reliableTalent;
+  const advantage = this.getFlag("dnd5e", "concentrationAdvantage") && !event?.ctrlKey;
+  if (advantage) rollConfig.advantage = advantage;
+  const concentrationBonus = this.getFlag("dnd5e", "concentrationBonus");
+  if (concentrationBonus && Roll.validate(concentrationBonus)) {
+    rollConfig.concentrationBonus = [concentrationBonus];
+  } else rollConfig.concentrationBonus = [];
+
+  const parts = foundry.utils.duplicate(options.parts ?? []);
+  foundry.utils.mergeObject(rollConfig, options);
+  delete options.parts;
+
+  // battle the clobbering.
+  if (parts?.length > 0) rollConfig.concentrationBonus.concat(...parts);
 
   // options should always have 'targetValue'.
   // ability is always passed in the event listeners.
