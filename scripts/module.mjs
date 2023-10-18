a358ff64465e3a4b555ae9ebfaf6284780f1b18e
Zhell <zhellqol@gmail.com>
2023-10-18 18:35:20 +0200

    Rewrite 4

--- scripts/module.mjs
+++ scripts/module.mjs
@@ -264,6 +264,75 @@ class Module {
   /* ---------------------------- */
 
   /**
+   * Redisplay the chat card of the item being concentrated on. If the item is a spell, display it at the correct level.
+   * @param {Token5e|TokenDocument5e|Actor5e} caster      The token, token document, or actor that is concentrating.
+   * @returns {Promise<ChatMessage>}
+   */
+  static async redisplayCard(caster) {
+    const actor = Module._getActor(caster);
+    if (!actor) return null;
+    const isConc = Module.isActorConcentrating(actor);
+    if (!isConc) {
+      ui.notifications.warn(game.i18n.format("CN.WarningActorNotConcentrating", {name: actor.name}));
+      return null;
+    }
+
+    const data = isConc.flags[Module.ID].data;
+    const item = await fromUuid(data.castData.itemUuid);
+    let clone;
+    if (item) clone = item.clone(data.itemData, {keepId: true});
+    else clone = new Item.implementation(data.itemData, {parent: actor});
+    if (clone.type === "spell") clone.updateSource({"system.level": data.castData.castLevel});
+
+    if (!clone) {
+      ui.notifications.warn("CN.ItemNotFound", {localize: true});
+      return null;
+    }
+
+    clone.prepareData();
+    clone.prepareFinalAttributes();
+    return clone.displayCard({});
+  }
+
+  /**
+   * Wait for concentration to be applied on an actor, optionally on a specific item.
+   * @param {Token5e|TokenDocument5e|Actor5e} caster      The token, token document, or actor to wait for.
+   * @param {Item5e} [item]                               The optional specific item.
+   * @param {number} [max_wait=10000]                     The maximum time to wait.
+   * @returns {Promise<ActiveEffect5e|boolean>}           The effect, if concentrating, otherwise false.
+   */
+  static async waitForConcentrationStart(caster, {item, max_wait = 10000} = {}) {
+    const actor = Module._getActor(caster);
+    if (!actor) return false;
+    const getConc = item ? Module.isActorConcentratingOnItem : Module.isActorConcentrating;
+
+    let conc = getConc(actor, item);
+    let waited = 0;
+    while (!conc && (waited < max_wait)) {
+      await new Promise(r => setTimeout(r, 100));
+      waited = waited + 100;
+      conc = getConc(actor, item);
+    }
+    return conc ?? false;
+  }
+
+  /**
+   * End all concentration effects on an actor.
+   * @param {Token5e|TokenDocument5e|Actor5e} caster      The token, token document, or actor to end concentration.
+   * @param {boolean} [message=true]                      Whether to display a message.
+   * @returns {Promise<ActiveEffect5e[]>}                 The array of deleted effects.
+   */
+  static async breakConcentration(caster, {message = true} = {}) {
+    const actor = Module._getActor(caster);
+    if (!actor) return [];
+    const ids = actor.appliedEffects.reduce((acc, e) => {
+      if (Module.isEffectConcentration(e)) acc.push(e.id);
+      return acc;
+    }, []);
+    return actor.deleteEmbeddedDocuments("ActiveEffect", ids, {concMessage: message});
+  }
+
+  /**
    * Is this effect a concentration effect?
    * @param {ActiveEffect5e} effect
    * @returns {boolean}
@@ -293,6 +362,17 @@ class Module {
   static isActorConcentrating(caster) {
     const actor = Module._getActor(caster);
     return actor ? actor.appliedEffects.find(e => e.statuses.has(Module.STATUS)) : false;
+  }
+
+  /**
+   * Determine whether an actor is concentrating on a specific item.
+   * @param {Token5e|TokenDocument5e|Actor5e} caster      The token, token document, or actor to test.
+   * @param {Item5e} item                                 The item to test concentration upon.
+   * @returns {ActiveEffect5e|boolean}                    The effect, if concentrating, otherwise false.
+   */
+  static isActorConcentratingOnItem(caster, item) {
+    const actor = Module._getActor(caster);
+    return actor ? actor.appliedEffects.find(e => item.uuid === e.flags[Module.ID]?.data?.castData?.itemUuid) : false;
   }
 
   /**
